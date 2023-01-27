f169aafc3b9415d9d268ccafd6a6258dc3d4e6a3
leonardo.nascimento <leonardo8544@gmail.com>
2023-01-27 17:09:01 -0300

    Deploy React app to Github Pages

---
+++ precache-manifest.e5113006162b4e19d727b5de43101fd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7daabc83f22786a9302",
+    "url": "/BarbosaLeo.github.io/static/css/main.5349a003.chunk.css"
+  },
+  {
+    "revision": "b7daabc83f22786a9302",
+    "url": "/BarbosaLeo.github.io/static/js/main.e5533213.chunk.js"
+  },
+  {
+    "revision": "2b968c7fd5c36c52508c",
+    "url": "/BarbosaLeo.github.io/static/js/runtime~main.57627934.js"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/BarbosaLeo.github.io/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "764637f8ea692a538740e8e93126a754",
+    "url": "/BarbosaLeo.github.io/index.html"
+  }
+];
