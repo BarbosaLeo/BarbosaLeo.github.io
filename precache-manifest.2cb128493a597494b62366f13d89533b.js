a478dadb1e6cdb47a7dafe4f25caeb51991fb774
leonardo.nascimento <leonardo8544@gmail.com>
2023-01-27 18:31:41 -0300

    Deploy React app to Github Pages

---
+++ precache-manifest.2cb128493a597494b62366f13d89533b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7daabc83f22786a9302",
+    "url": "/static/css/main.5349a003.chunk.css"
+  },
+  {
+    "revision": "b7daabc83f22786a9302",
+    "url": "/static/js/main.e5533213.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "dd116ddbbad596776d454af9bf896ba9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BarbosaLeo.github.io/precache-manifest.74e3ab38e51bef78c921dd78492c5231.js"
+  "/precache-manifest.2cb128493a597494b62366f13d89533b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/BarbosaLeo.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
